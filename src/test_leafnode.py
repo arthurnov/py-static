e7222ce7d8043539e1f9f363b129c8b9beb96c4a
arthurnov <art.dsky@gmail.com>
2025-01-16 00:00:39 +0200

    ch2 l4

---
+++ src/test_leafnode.py
@@ -0,0 +1,23 @@
+import unittest
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(
+            '<p>This is a paragraph of text.</p>', node.to_html()
+        )
+    
+    def test_eq2(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            '<a href="https://www.google.com">Click me!</a>', node.to_html()
+        )
+
+    def test_repr(self):
+        node = LeafNode("p", "This is a paragraph of text.", "")
+        self.assertEqual(
+            "LeafNode(p, This is a paragraph of text., )", repr(node)
+        )
+if __name__ == "__main__":
+    unittest.main()
