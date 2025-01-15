b459c3d239d4eb102bade0636152af8fb8641be0
arthurnov <art.dsky@gmail.com>
2025-01-15 22:54:05 +0200

    ch2 l2

---
+++ src/test_textnode.py
@@ -0,0 +1,38 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2, "texts should be equal")
+    
+    def test_eq_false_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node!", TextType.BOLD)
+        self.assertNotEqual(node, node2, "texts should not be equal")
+    
+    def test_eq_false_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.NORMAL)
+        self.assertNotEqual(node, node2, "types should not be equal")
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "www.google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.google.com")
+        self.assertEqual(node, node2, "urls should be equal")
+    
+    def test_eq_false_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.google.com")
+        self.assertNotEqual(node, node2, "urls should not be equal")
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.NORMAL, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, TextType.NORMAL, https://www.boot.dev)", repr(node)
+        )
+
+if __name__ == "__main__":
+    unittest.main()
