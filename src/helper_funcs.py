c35c8d8356d61dbf75dd7fce8d10172574e797f8
arthurnov <art.dsky@gmail.com>
2025-01-19 20:55:44 +0200

    moving pcs

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -4,13 +4,20 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     print(f"INPUT NODES: {old_nodes}")
     for node in old_nodes:
-        sentance = ""
         print(f"NODE: {node}")
+        index = node.text.find(delimiter)
+        print(f"JUST TAGGED: {index}")
+        new_nodes.append(node.text.split(delimiter))
         
-    new_nodes.append(old_nodes[0].text.split(delimiter, maxsplit=1))
     return new_nodes
+
+def rec(text, deli):
+    if text.count(deli) < 2:
+        retur
 
 node = TextNode("This is text with a `code block` word", TextType.NORMAL)
 node2 = TextNode("This is another text with a `code block` word, maybe even `two code blocks`", TextType.NORMAL)
+node3 = TextNode("And this is text with a **bold** word", TextType.NORMAL)
 new_nodes = split_nodes_delimiter([node, node2], "`", TextType.CODE)
+new_nodes.extend(split_nodes_delimiter([node3], "**", TextType.BOLD))
 print(new_nodes)
