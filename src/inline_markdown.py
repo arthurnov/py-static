1abfbf43f537205285f6321eb11a3adc05334239
arthurnov <art.dsky@gmail.com>
2025-01-20 17:23:54 +0200

    end of day

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -21,6 +21,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.IMAGE:
+            new_nodes.append(node)
+            continue
+        split_nodes = []
+        
+
+def split_node_link(old_nodes):
+    pass
+
 def extract_markdown_images(text):
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
