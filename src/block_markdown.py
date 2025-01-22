0c13e9dafcc068f09da2814de9da62276be6232a
arthurnov <art.dsky@gmail.com>
2025-01-22 23:41:33 +0200

    nothing is working and i am tired

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,4 @@
+import re
 # def markdown_to_blocks(markdown):
 #     blocks = []
 #     blocks = markdown.split("\n\n")
@@ -13,3 +14,30 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    print(lines)
+    if re.findall(r"^#{1,6} ", block) != []:
+        return "heading"
+    if re.findall(r"^`{3}\n", block) != [] and re.findall(r"\n`{3}$", block) != []:
+        return "code"
+    if re.findall(r"^[^>]", block) == [] and re.findall(r"\n[^>]", block) == []:
+        return "quote"
+    if (re.findall(r"^[^(\* )]", block) == [] and re.findall(r"\n[^(\* )]", block) == []) or (re.findall(r"^[^(- )]", block) == [] and re.findall(r"\n[^(- )]", block) == []):
+        return "unordered_list"
+    return "paragraph"
+
+
+text = '''* ijfeijf
+* jiejfirfj
+ ijirj
+'''
+print(block_to_block_type(text))
+
+# paragraph
+# heading
+# code
+# quote
+# unordered_list
+# ordered_list
