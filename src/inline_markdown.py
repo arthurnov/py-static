4914cceca689bec8b333d76bec3ca85a77731295
arthurnov <art.dsky@gmail.com>
2025-01-22 11:06:33 +0200

    ch3 l5

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -23,29 +23,55 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
 def split_nodes_image(old_nodes):
     new_nodes = []
-    for node in old_nodes:
-        if node.text_type == TextType.IMAGE:
-            new_nodes.append(node)
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.NORMAL:
+            new_nodes.append(old_node)
             continue
-        split_nodes = []
-        new_nodes.append(extract_markdown_images(node.text))
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.NORMAL))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.NORMAL))
     return new_nodes
-        
+
 
 def split_nodes_link(old_nodes):
     new_nodes = []
-    for node in old_nodes:
-        if node.text_type == TextType.LINK:
-            new_nodes.append(node)
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.NORMAL:
+            new_nodes.append(old_node)
             continue
-        # print(node.text)
-        sections = []
-        list_of_tups = extract_markdown_links(node.text)
-        print(list_of_tups)
-        for tup in list_of_tups:
-            link_text, link_href = tup
-            sections.append(node.text.split(f"[{link_text}]({link_href})", 1))
-            print(f"SECTIONS: {sections}")
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.NORMAL))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.NORMAL))
     return new_nodes
 
 def extract_markdown_images(text):
@@ -57,13 +83,6 @@ def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
     return matches
-
-node = TextNode(
-    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
-    TextType.NORMAL,
-)
-new_nodes = split_nodes_link([node])
-print(new_nodes)
 
 # OG approach, switched to one provided by boot.dev
 # def split_nodes_delimiter(old_nodes, delimiter, text_type):
