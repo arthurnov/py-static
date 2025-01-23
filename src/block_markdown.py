9341bac3d75015d30fb702215f46df5e67a66f35
arthurnov <art.dsky@gmail.com>
2025-01-23 16:01:36 +0200

    ch4 l2

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -5,6 +5,13 @@ import re
 #     blocks = list(map(lambda x: x.strip(), blocks))
 #     return blocks
 
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -17,23 +24,26 @@ def markdown_to_blocks(markdown):
 
 def block_to_block_type(block):
     lines = block.split("\n")
-    print(lines)
+    ul = True
+    ol = True
     if re.findall(r"^#{1,6} ", block) != []:
-        return "heading"
+        return block_type_heading
     if re.findall(r"^`{3}\n", block) != [] and re.findall(r"\n`{3}$", block) != []:
-        return "code"
+        return block_type_code
     if re.findall(r"^[^>]", block) == [] and re.findall(r"\n[^>]", block) == []:
-        return "quote"
-    if (re.findall(r"^[^(\* )]", block) == [] and re.findall(r"\n[^(\* )]", block) == []) or (re.findall(r"^[^(- )]", block) == [] and re.findall(r"\n[^(- )]", block) == []):
-        return "unordered_list"
-    return "paragraph"
-
-
-text = '''* ijfeijf
-* jiejfirfj
- ijirj
-'''
-print(block_to_block_type(text))
+        return block_type_quote
+    for line in lines:
+        if re.findall(r"^\* ", line) == [] and re.findall(r"^- ", line) == []:
+            ul = False
+    if ul == True:
+        return block_type_ulist
+    for i in range(len(lines)):
+        regex = r"^" + str(i+1) + ". "
+        if re.findall(regex, lines[i]) == []:
+            ol = False
+    if ol == True:
+        return block_type_olist
+    return block_type_paragraph
 
 # paragraph
 # heading
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,29 +1,74 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_olist,
+    block_type_ulist,
+    block_type_quote,
+)
 
-class TestBlockMarkdown(unittest.TestCase):
-    def test_site_example(self):
-        markdown = '''# This is a heading
 
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+class TestMarkdownToHTML(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
 
-* This is the first list item in a list block
-* This is a list item
-* This is another list item'''
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
 
-        self.assertListEqual(
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
             [
-                '# This is a heading',
-                'This is a paragraph of text. It has some **bold** and *italic* words inside of it.',
-                '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",
             ],
-            markdown_to_blocks(markdown)
         )
 
-    def test_site_example(self):
-        markdown = ""
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
 
-        self.assertListEqual(
-            [],
-            markdown_to_blocks(markdown)
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",
+            ],
         )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), block_type_ulist)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), block_type_olist)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
+
+
+if __name__ == "__main__":
+    unittest.main()
